e519e013b95670b623ef8bf48d457eb7f9e62a97
Grootbaba <85894783+Grootbaba@users.noreply.github.com>
2021-06-27 10:17:14 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,10 +25,11 @@ HEROKU_URL = os.environ['HEROKU_URL']
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Enter the link to convert in your affiliate link")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Hola! Este bot responde a los enlaces de amazon añadiendo un codigo de afiliado!")
 
 # Create the new URL with the refer tag
-
+def newReferURL(pcode):
+    return baseURL+pcode+"?tag="+affiliate_tag
 
 #Expand shorted URL (amzn.to links) to normal Amazon URL
 def unshortURL(url):
@@ -38,21 +39,19 @@ def unshortURL(url):
 
 #Filter the msg text to extract the URL if found. Then send the corresponding reply
 # with the new affiliate URL
-
 def filterText(update, context):
-  url1 = re.sub('&tag=.*?&','&tag=tricksburner02-21&',resp.url, flags=re.DOTALL)
-#     pCode=""
-#     msg = update.message.text
-#     start = msg.find("amzn.to")
-#     if start!=-1:
-#         msg = unshortURL(msg[start:].split()[0])
-#     start = msg.find(baseURL)
-#     if start != -1:
-#         #Regular expression to extract the product code. Adjust if different URL schemes are found.
-#         m = re.search(r'(?:dp\/[\w]*)|(?:gp\/product\/[\w]*)',msg[start:].split(" ")[0])
-#         if m != None:
-#             pCode = m.group(0)
-        context.bot.send_message(chat_id=update.message.chat_id,reply_to_message_id=update.message.message_id, text=url1)
+    pCode=""
+    msg = update.message.text
+    start = msg.find("amzn.to")
+    if start!=-1:
+        msg = unshortURL(msg[start:].split()[0])
+    start = msg.find(baseURL)
+    if start != -1:
+        #Regular expression to extract the product code. Adjust if different URL schemes are found.
+        m = re.search(r'(?:dp\/[\w]*)|(?:gp\/product\/[\w]*)',msg[start:].split(" ")[0])
+        if m != None:
+            pCode = m.group(0)
+        context.bot.send_message(chat_id=update.message.chat_id,reply_to_message_id=update.message.message_id, text=newReferURL(pCode))
 
 def main():
     """Start the bot."""
